db4b1b229d0456b467407ddddd2ab67f3164f0b3
Aleksey <ale.anasov94@gmail.com>
2019-01-30 22:30:32 +0300

    Применил стили и подключил bootstrap

--- index.php
+++ index.php
@@ -3,16 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Мой первый блог</title>
+    <!--Стили bootstrap-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <!--Стили собственные-->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <div>
+    <div class="container">
         <h1>Мой первый блог</h1>
-        <div>
+        <div class="article">
             <h3>Заголовок 1</h3>
             <em>Опубликовано: 2019-01-30</em>
             <p>Статья 1</p>
         </div>
-        <div>
+        <div class="article">
             <h3>Заголовок 2</h3>
             <em>Опубликовано: 2019-01-30</em>
             <p>Статья 2</p>
