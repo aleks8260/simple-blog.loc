8ba1a5a7e7b5a5539323b4ffde37670fe55b52a3
Aleksey <ale.anasov94@gmail.com>
2019-01-30 22:23:53 +0300

    Разметка страницы

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Мой первый блог</title>
+</head>
+<body>
+    <div>
+        <h1>Мой первый блог</h1>
+        <div>
+            <h3>Заголовок 1</h3>
+            <em>Опубликовано: 2019-01-30</em>
+            <p>Статья 1</p>
+        </div>
+        <div>
+            <h3>Заголовок 2</h3>
+            <em>Опубликовано: 2019-01-30</em>
+            <p>Статья 2</p>
+        </div>
+    </div>
+    <footer>
+        <p>&copy; 2019. Мой первый Блог</p>
+    </footer>
+</body>
+</html>
